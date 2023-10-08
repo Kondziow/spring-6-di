7dceb2d4665bb5ef5b7cc8c85b8daefef2b8057f
Kondziow <konradwojtanowski02@gmail.com>
2023-10-08 14:35:34 +0200

    DI with Spring

--- src/main/java/guru/springframework/spring6di/controllers/SetterInjectionController.java
+++ src/main/java/guru/springframework/spring6di/controllers/SetterInjectionController.java
@@ -1,12 +1,17 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectionController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
+        System.out.println("SetterInjecitonController.setGreetingService");
         this.greetingService = greetingService;
     }
 
